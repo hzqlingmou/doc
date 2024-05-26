9f2575a946af98ddf3a7ec04eec744feaaac4802
EscapedSpark <113490727+hzqlingmou@users.noreply.github.com>
2024-05-26 09:39:16 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -76,7 +76,11 @@ export default hopeTheme({
       playground: {
         presets: ["ts", "vue"],
       },
-      
+          
+      sub: true,
+      sup: true,
+      tabs: true,
+      vPre: true,
       stylize: [
       // 样式化
         {
@@ -87,105 +91,11 @@ export default hopeTheme({
                 tag: "Badge",
                 attrs: { type: "tip" },
                 content: "Recommended",
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-
-      // Install chart.js before enabling it
-      // chart: true,
-
-      // insert component easily
-
-      // Install echarts before enabling it
-      // echarts: true,
-
-      // Install flowchart.ts before enabling it
-      // flowchart: true,
-
-      // gfm requires mathjax-full to provide tex support
-      // gfm: true,
-
-      // Install katex before enabling it
-      // katex: true,
-
-      // Install mathjax-full before enabling it
-      // mathjax: true,
-
-      // Install mermaid before enabling it
-      // mermaid: true,
-
-      // playground: {
-      //   presets: ["ts", "vue"],
-      // },
-
-      // Install reveal.js before enabling it
-      // revealJs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-
-      // Install @vue/repl before enabling it
-      // vuePlayground: true,
-
-      // Install sandpack-vue3 before enabling it
-      // sandpack: true,
+            };
+          },
+        },
+      ],
     },
-
-    // Install @vuepress/plugin-pwa and uncomment these if you want a PWA
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cachePic: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
   },
 });
+
