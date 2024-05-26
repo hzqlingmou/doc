ecf3b7b729514e699762c6bc85e205f56fed98ed
EscapedSpark <113490727+hzqlingmou@users.noreply.github.com>
2024-05-26 09:12:11 +0800

    Update zh.ts

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,12 +1,12 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const zhSidebar = sidebar({
-  "/zh/": [
-    "",
+  "/": [
+    "主页",
     {
       text: "文档",
       icon: "book",
-      prefix: "guide/",
+      prefix: "/guide/",
       children: "structure",
     },
   ],
